e316a65dcb71dda2ffdedc00cf96ddcb84dd0b18
Govardhan K G <govi.reddy420@gmail.com>
2025-11-05 10:18:01 +0530

    added otp logic and twilio api

--- src/routes/v1/user.ts
+++ src/routes/v1/user.ts
@@ -1,53 +1,51 @@
 import { Router } from "express";
-import {generateKey,generateToken, verifyToken} from "authenticator"
-const router=Router()
-import 'dotenv/config'; 
+import { generateKey, generateToken, verifyToken } from "authenticator"
+const router = Router()
+import 'dotenv/config';
 import jwt from "jsonwebtoken";
 import { PrismaClient } from "@prisma/client";
 import { JWT_PASSWORD } from "../../config.js";
- const client=new PrismaClient()
-router.post("/signup",async(req,res)=>{
-	const number=req.body.phone_Number
-	const totp= generateToken(number + "SIGNUP")
-	client.user.upsert({
-		where:{
-			number
-		},
-		create:{
-			number
-		},
-		update:{
+import { sendMessage } from "../../utils/twilio.js";
+const client = new PrismaClient()
 
-		}
+router.post("/signup", async (req, res) => {
+	const number = req.body.phone_Number
+	const totp = generateToken(number + "SIGNUP")
+	await client.user.upsert({
+		where: {number},
+		create: {number},
+		update: {}
 	})
-	if (process.env.NODE_ENV=="production") {
+	console.log("DEBUG: Sending OTP to number =>", number);
+	if (process.env.NODE_ENV == "production") {
+		try {
+			await sendMessage(`your OTP for logging in is ${totp}`, number)
 
+		} catch (error) {
+			console.error("Twilio error:", error);
+			throw new Error("error from twilio");
+		}
 	}
 	res.json({
 		totp
 	})
 
 })
-router.post("/signup/verify",async(req,res)=>{
-	const number=req.body.phone_Number
-	const name=req.body.name
-	const totp= generateToken(number + "SIGNUP")
-	if (!verifyToken(number + "SIGNUP" ,req.body.otp)) {
+router.post("/signup/verify", async (req, res) => {
+	const number = req.body.phone_Number
+	const name = req.body.name
+	const totp = generateToken(number + "SIGNUP")
+	if (!verifyToken(number + "SIGNUP", req.body.otp)) {
 		res.json({
-			message:"invalid token"
+			message: "invalid token"
 		})
 		return
 	}
 	const UserId=await client.user.update({
-		where:{
-			number
-		},
-		data:{
-			verified:true,
-			name
-		}
-	})
-	const token=jwt.sign({UserId},JWT_PASSWORD)
+    where: { number },
+    data: { verified: true, name },
+  });
+	const token = jwt.sign({ UserId }, JWT_PASSWORD)
 	res.json({
 		token
 	})
